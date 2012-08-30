7510bdd91a183aa9de11a8914924e5cea9484b89
michelleg <mgao@uchicago.edu>
2012-08-29 23:42:08 -0500

    Make a basic User Model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
-class ApplicationController < ActionController::Base
+#Class ActionController::TestCase
+#include Devise::TestHelpers
+#end
+class ApplicationController < ActionController:: Base
   protect_from_forgery
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 SampleApp::Application.routes.draw do
-  #get "users/new"
 
 root :to => 'static_pages#home'
 match '/help',   to: 'static_pages#help'
