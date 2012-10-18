0120d791137d57ed62eb931bb51c9a12c1262bcf
michelleg <mgao@uchicago.edu>
2012-10-18 01:33:35 -0500

    change help file to favorite file

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,71 +7,71 @@ describe "Static Pages" do
 	shared_examples_for "all static pages" do
 		it { should have_selector('h1',    text: heading)}
 		it { should have_selector('title', text: full_title(page_title))}
-    end
+  end
+
+  describe "Home Page" do
   
-    describe "Home Page" do
-    
-	    before { visit root_path }
-	    let(:heading)   { 'Twitter App'}
-	    let(:page_title) { '' }
+    before { visit root_path }
+    let(:heading)   { 'Twitter App'}
+    let(:page_title) { '' }
 
 		it_should_behave_like "all static pages"
-	    it { should_not have_selector( 'title', text: '| Home' ) }
+    it { should_not have_selector( 'title', text: '| Home' ) }
 
-	    describe "for signed-in users " do
-	    	let(:user) { FactoryGirl.create(:user)}
-	    	before do
-	    		FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
-	    		FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
-	    		sign_in user
-	    		visit root_path
-	    	end
+    describe "for signed-in users " do
+    	let(:user) { FactoryGirl.create(:user)}
+    	before do
+    		FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+    		FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+    		sign_in user
+    		visit root_path
+    	end
 
-	    	it "should render the user's feed"  do
-	    		user.feed.each do |item|
-	    			page.should have_selector("li##{item.id}", text: item.content)
-	    		end
-	    	end
+    	it "should render the user's feed"  do
+    		user.feed.each do |item|
+    			page.should have_selector("li##{item.id}", text: item.content)
+    		end
+    	end
 
-	    	describe "follower/following counts" do
-	    		let(:other_user) { FactoryGirl.create(:user)}
-	    		before do
-	    			other_user.follow!(user)
-	    			visit root_path
-	    		end
+    	describe "follower/following counts" do
+    		let(:other_user) { FactoryGirl.create(:user)}
+    		before do
+    			other_user.follow!(user)
+    			visit root_path
+    		end
 
-	    		it { should have_link("0 following", href: following_user_path(user))}
-	    		it { should have_link("1 followers", href: followers_user_path(user))}
-	    		
-	    	end
-	    	
-	    end
+    		it { should have_link("0 following", href: following_user_path(user))}
+    		it { should have_link("1 followers", href: followers_user_path(user))}
+    		
+    	end
+    	
     end
+  end
 
 
 	describe "Favorite Page" do
 
 		before { visit favorite_path }
-        let(:heading) { 'Favorite'}
-        let(:page_title) { 'Favorite'}
-        it_should_behave_like "all static pages"
+    let(:heading) { 'Favorite'}
+    let(:page_title) { 'Favorite'}
+    it_should_behave_like "all static pages"
 		
 	end
 	
 	describe "About page" do
 		before { visit about_path }
-        let(:heading) { 'About'}
-        let(:page_title) { 'About'}
-        it_should_behave_like "all static pages"
+    let(:heading) { 'About'}
+    let(:page_title) { 'About'}
+    it_should_behave_like "all static pages"
 		
 	end
 
 	describe "Contact Page" do
 
 		before  { visit contact_path }
-        let(:heading) { 'Contact'}
-        let(:page_title) { 'Contact'}
-        it_should_behave_like "all static pages"
+    let(:heading) { 'Contact'}
+    let(:page_title) { 'Contact'}
+    it_should_behave_like "all static pages"
 		
 	end
 	it "should have the right links on the layout" do
@@ -85,6 +85,7 @@ describe "Static Pages" do
 		click_link "Contact"
 		page.should have_selector 'title', text: full_title('Contact')
 		click_link "Home"
+		page.should have_selector 'title', text: full_title('')
 		click_link "Sign up now!"
 		page.should have_selector 'title', text: full_title('Sign up')
 		click_link "Twitter app"
